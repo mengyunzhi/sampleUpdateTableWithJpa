fbf89e60f3175735c701a0872dd316e52346c8c3
panjie <3792535@qq.com>
2018-07-02 07:57:15 +0800

    INIT

---
+++ src/main/java/com/mengyunzhi/sampleupdatetablewithjpa/SampleUpdateTableWithJpaApplication.java
@@ -0,0 +1,12 @@
+package com.mengyunzhi.sampleupdatetablewithjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleUpdateTableWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleUpdateTableWithJpaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mengyunzhi/sampleupdatetablewithjpa/SampleUpdateTableWithJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mengyunzhi.sampleupdatetablewithjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleUpdateTableWithJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
